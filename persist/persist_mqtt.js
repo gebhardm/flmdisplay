66dfc5c10511ce0aa91859cf694e35c7277e8782
Markus Gebhard <markus.gebhard@web.de>
2014-06-18 22:05:12 +0200

    persist on mdns

--- persist/persist_mqtt.js
+++ persist/persist_mqtt.js
@@ -9,29 +9,26 @@ delivered "as is", no guarantee to work ;-)
 uses
   mysql module: https://github.com/felixge/node-mysql
   mqtt module: https://github.com/adamvr/MQTT.js/
+  mdns module: https://github.com/agnat/node_mdns
 ************************************************************/
-var mysql = require('mysql');
-var mqtt  = require('mqtt');
+var mysql = require('mysql'); // database connect and query
+var mqtt  = require('mqtt');  // mqtt message client
+var mdns  = require('mdns');  // multicast DNS service discovery
 
-var mqttbroker = '192.168.0.50'; // provide your broker's address
-var mqttport   = 1883;           // the default port
-var mqttclient = mqtt.createClient(mqttport, mqttbroker);
-
-var database = mysql.createConnection(
-   {
-      host : 'localhost',
-      user : 'pi',
-      password : 'raspberry',
-      database : 'flm'
-   }
-);
-
-mqttclient.on('message', function(topic, payload) {
-   var subtopics = topic.split('/');
-   switch (subtopics[3]) {
+var mdnsbrowser = mdns.createBrowser(mdns.tcp('mqtt'));
+mdnsbrowser.on('serviceUp', function(service) {
+  console.log('detected:'+service.addresses[0]+':'+service.port);
+  // connect to discovered mqtt broker
+  var mqttclient = mqtt.createClient(service.port, service.addresses[0]);
+  // subscribe to sensor topics
+  mqttclient.subscribe('/sensor/#');
+  // act on received message
+  mqttclient.on('message', function(topic, payload) {
+    var subtopics = topic.split('/');
+    switch (subtopics[3]) {
       case 'gauge':
          var gauge = JSON.parse(payload);
-// FLM gauges consist of timestamp, value, and unit
+         // FLM gauges consist of timestamp, value, and unit
          if (gauge.length == 3) {
             // use the following conversion when using mySQL TIMESTAMP and replace gauge[0]
             //var date = new Date(gauge[0]*1000).toISOString().slice(0, 19).replace('T', ' ');
@@ -47,23 +44,35 @@ mqttclient.on('message', function(topic, payload) {
                           + ' value = VALUES(value),'
                           + ' unit = VALUES(unit);';
             database.query(insertStr, function(err, res) {
-              if (err) {
-                database.end();
-                throw err;
-              }
-            });
-         }
+              if (err) { database.end();
+                         throw err; }
+            }); // database.query
+         } // gauge length 3 - you may define further gauge lengths to be persisted
          break;
       case 'counter': break;
+    } // switch
+  }); // mqttclient.on
+}); // mdnsbrowser.on
+
+// and start to discover
+mdnsbrowser.start();
+
+// define database and connect
+var database = mysql.createConnection(
+   {
+      host : 'localhost',
+      user : 'pi',
+      password : 'raspberry',
+      database : 'flm'
    }
-});
+);
 
 database.connect(function(err) {
   if (err) throw err;
   console.log('Database flm successfully connected');
 });
 
-// create the persistence 
+// create the persistence if it does not exist
 var createTabStr = 'CREATE TABLE IF NOT EXISTS flmdata'
                  + '( sensor CHAR(32),'
                  + '  timestamp CHAR(10),'                 //'  timestamp TIMESTAMP,'
@@ -79,5 +88,3 @@ database.query(createTabStr, function(err, res) {
    }
    console.log('Create table successful...');
 });
-
-mqttclient.subscribe('/sensor/#');
