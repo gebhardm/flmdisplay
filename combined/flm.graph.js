02cfd0a478a402affff1e4c0091493c792a113a2
Markus Gebhard <markus.gebhard@web.de>
2014-07-08 19:59:15 +0200

    draft version of 5min graph display

--- combined/flm.graph.js
+++ combined/flm.graph.js
@@ -3,10 +3,9 @@ var socket = io.connect(location.host);
 // prepare graph display
 var series = new Array();
 var options = {
-xaxis: { mode: "time",
-timezone: "browser" },
-yaxis: { min: 0 },
-selection: { mode: "x" }
+                xaxis: { mode: "time",
+                         timezone: "browser" },
+                yaxis: { min: 0 }
 };
 // process socket connection
 socket.on('connect', function () {
@@ -22,15 +21,33 @@ socket.on('connect', function () {
 		// now compute the gauge
 		switch (area) {
 		case 'gauge':
+                        // process currently only the FLM delivered values with timestamp
 			if (value.length == 3) {
+                                // check if current sensor was already registered
 				var obj = series.filter(function(o) { return o.label == sensor; });
+                                // ...if not, register it
 				if (obj[0] == null) {
 					obj ={};
 					obj.label = sensor;
 					obj.data  = [value[0], value[1]];
 					series.push(obj);
 				}
-				else obj[0].data.push([value[0], value[1]]);
+				// ...otherwise, push the current value
+				else { 
+                                        obj[0].data.push([value[0], value[1]]);
+                                        // reduce the datavolume to n values per series
+                                        if (obj[0].data.length > 300) {
+                                                var limit = obj[0].data[0][0];
+                                                var selGraph = new Array();
+                                                for (var i in series) {
+                                                        var selObj = {};
+                                                        selObj.label = series[i].label;
+                                                        selObj.data  = series[i].data.filter(function(v) {return v[0] > limit;});
+                                                        selGraph.push(selObj);
+                                                }
+                                                series = selGraph;
+                                        }
+                                }
 				$("#graph").plot(series, options);
 			} // if length
 			break;
@@ -46,4 +63,9 @@ $(document).ready(function() {
 	$("#sel_cnt").click( function() { window.location = 'panel.html'; });
 	$("#sel_gph").click( function() { window.location = 'graph.html'; });
 	$("#sel_cht").click( function() { window.location = 'chart.html'; });
+        var offset = 20; //px
+        var width = $(document).width();
+        width -= offset * 2;
+        var height = width * 3 / 4;
+        $("#graph").width(width).height(height).offset({left:offset});
 });
