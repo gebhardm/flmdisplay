03982b56e7ae592f7d095ac45a13f0996fac1db2
Markus Gebhard <markus.gebhard@web.de>
2014-10-19 18:13:17 +0200

    decouple button handling

--- combined/flm.gauge.js
+++ combined/flm.gauge.js
@@ -76,21 +76,3 @@ socket.on('connect', function () {
 		topic : '/sensor/#'
 	});
 });
-$(document).ready(function () {
-	// Selection button handling
-        $("#sel_gge").click(function () {
-                window.location = 'gauge.html';
-        });
-	$("#sel_pnl").click(function () {
-		window.location = 'index.html';
-	});
-	$("#sel_cnt").click(function () {
-		window.location = 'panel.html';
-	});
-	$("#sel_gph").click(function () {
-		window.location = 'graph.html';
-	});
-	$("#sel_cht").click(function () {
-		window.location = 'chart.html';
-	});
-});
