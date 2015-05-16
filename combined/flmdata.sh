c6d0d35759f600407f968cc9f3d84a83a40dcc28
Markus Gebhard <markus.gebhard@web.de>
2015-05-16 19:54:00 +0200

    node module update

--- combined/flmdata.sh
+++ combined/flmdata.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export AVAHI_COMPAT_NOWARN=1
-node serve_flmdata.js &
+# start the script with "no hang-up" to detect potential uncaught errors
+# to have no output catching, just delete the "nohup"
+nohup node serve_flmdata.js &
