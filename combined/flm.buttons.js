1c2ae9fe4e1e83e65decfb6ec6234bd151287fea
Markus Gebhard <markus.gebhard@web.de>
2015-08-24 09:28:32 +0200

    preparation for adding a simple consumption view

--- combined/flm.buttons.js
+++ combined/flm.buttons.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
-	$(document.body).append("<div class=\"panel-body\" align=\"center\" id=\"buttons\"> \
+	$(document.body).append("<div class=\"panel-body\" align=\"center\"> \
+	<button class=\"btn btn-primary btn-sm\" id=\"sel_chart\">Chart</button> \
+	<button class=\"btn btn-primary btn-sm\" id=\"sel_consumption\">Consumption</button> \
 	<button class=\"btn btn-primary btn-sm\" id=\"sel_gauge\">Gauge</button> \
 	<button class=\"btn btn-primary btn-sm\" id=\"sel_graph\">Graph</button> \
 	<button class=\"btn btn-primary btn-sm\" id=\"sel_panel\">Panel</button> \
-	<button class=\"btn btn-primary btn-sm\" id=\"sel_chart\">Chart</button>\
 	</div>");
     // Selection button handling
+    $("#sel_chart").click(function() {
+        window.location = "chart.html";
+    });
+    $("#sel_consumption").click(function() {
+        window.location = "consumption.html";
+    });
     $("#sel_gauge").click(function() {
-        window.location = "index.html";
+        window.location = "gauge.html";
     });
     $("#sel_panel").click(function() {
         window.location = "panel.html";
     });
     $("#sel_graph").click(function() {
         window.location = "graph.html";
-    });
-    $("#sel_chart").click(function() {
-        window.location = "chart.html";
     });
 });
