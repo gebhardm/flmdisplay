1c8e2614f866520e6404f6fa251a4aa84f1ba86f
Markus Gebhard <markus.gebhard@web.de>
2015-08-24 09:21:17 +0200

    centralized button handling for easier additions

--- combined/flm.buttons.js
+++ combined/flm.buttons.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
+	$(document.body).append("<div class=\"panel-body\" align=\"center\" id=\"buttons\"> \
+	<button class=\"btn btn-primary btn-sm\" id=\"sel_gauge\">Gauge</button> \
+	<button class=\"btn btn-primary btn-sm\" id=\"sel_graph\">Graph</button> \
+	<button class=\"btn btn-primary btn-sm\" id=\"sel_panel\">Panel</button> \
+	<button class=\"btn btn-primary btn-sm\" id=\"sel_chart\">Chart</button>\
+	</div>");
     // Selection button handling
-    $("#sel_gge").click(function() {
-        window.location = "gauge.html";
+    $("#sel_gauge").click(function() {
+        window.location = "index.html";
     });
-    $("#sel_pnl").click(function() {
+    $("#sel_panel").click(function() {
         window.location = "panel.html";
     });
-    $("#sel_gph").click(function() {
+    $("#sel_graph").click(function() {
         window.location = "graph.html";
     });
-    $("#sel_cht").click(function() {
+    $("#sel_chart").click(function() {
         window.location = "chart.html";
     });
 });
