e2d62fea40c22ad7ebde917231a5f8ad1e9b0be0
Markus Gebhard <markus.gebhard@web.de>
2015-08-31 20:56:19 +0200

    consumption view like in flmlocal

--- combined/flm.consumption.js
+++ combined/flm.consumption.js
@@ -1,6 +1,8 @@
 // link to the web server's IP address for socket connection
 var socket = io.connect(location.host);
 
+var cfgVis = true;
+
 socket.on("connect", function() {
     // emit the subscription
     socket.emit("subscribe", {
@@ -11,35 +13,6 @@ socket.on("connect", function() {
     });
     // objects containing the actual sensor data
     var sensors = {};
-    var limit = 3600;
-    // initialize the consumption gauges
-    var grid = new JustGage({
-        id: "grid",
-        value: 0,
-        title: "Grid",
-        label: "W",
-        min: 0,
-        max: limit,
-        decimals: 0
-    });
-    var production = new JustGage({
-        id: "production",
-        value: 0,
-        title: "Production",
-        label: "W",
-        min: 0,
-        max: limit,
-        decimals: 0
-    });
-    var consumption = new JustGage({
-        id: "consumption",
-        value: 0,
-        title: "Consumption",
-        label: "W",
-        min: 0,
-        max: limit,
-        decimals: 0
-    });
     // handle the received MQTT messages
     socket.on("mqtt", function(msg) {
         // split the received message at the slashes
@@ -119,15 +92,7 @@ socket.on("connect", function() {
             }
             // now build the gauge display
             if (sensor.type == null && sensor.unit === "W") {
-                $("#choices").append("<div class='form-inline'>" + 
-                                     "<label for='type " + sensor.name + 
-                                     "' class='control-label col-sm-2'>" + sensor.name + 
-                                     "</label>" + 
-                                     "<select id='type " + sensor.name + "'>" + 
-                                     "<option>Consumption</option>" + 
-                                     "<option>Production</option>" + 
-                                     "</select>" + 
-                                     "</div>");
+                $("#choices").append("<div class='form-inline'>" + "<label for='type " + sensor.name + "' class='control-label col-sm-2'>" + sensor.name + "</label>" + "<select id='type " + sensor.name + "'>" + "<option>Consumption</option>" + "<option>Production</option>" + "</select>" + "</div>");
             }
             // compute the selected sensor type
             var selElt = document.getElementById("type " + sensor.name);
@@ -159,21 +124,59 @@ socket.on("connect", function() {
             }
         }
         var gridValue = consumptionValue - productionValue;
-        // update the gauges
-        if (gridValue > limit) {
-            grid.refresh(gridValue, gridValue);
+        var selfuseValue = productionValue > consumptionValue ? consumptionValue : productionValue;
+        var supplyValue = productionValue > consumptionValue ? productionValue - consumptionValue : 0;
+        var obtainedValue = consumptionValue - productionValue > 0 ? consumptionValue - productionValue : 0;
+        // compute the scaling
+        var scale = $("#image").width() / 1226;
+        var pos = $("#image").offset();
+        // format the output
+        $(".watt").css("position", "absolute");
+        $(".watt").css("width", 307 * scale + "px");
+        $(".watt").css("text-align", "center");
+        $(".watt").css("color", "rgb(91,155,213)");
+        $(".watt").css("font-family", "arial");
+        $(".watt").css("font-size", 64 * scale + "px");
+        $(".watt").css("font-weight", "bold");
+        //$(".watt").css("border", "solid grey 2px");
+        $("#grid").css("top", pos.top + 245 * scale + "px");
+        $("#grid").css("left", pos.left + 60 * scale + "px");
+        $("#supply").css("top", pos.top + 40 * scale + "px");
+        $("#supply").css("left", pos.left + 500 * scale + "px");
+        $("#selfuse").css("top", pos.top + 420 * scale + "px");
+        $("#selfuse").css("left", pos.left + 740 * scale + "px");
+        $("#production").css("top", pos.top + 245 * scale + "px");
+        $("#production").css("left", pos.left + 910 * scale + "px");
+        $("#consumption").css("top", pos.top + 795 * scale + "px");
+        $("#consumption").css("left", pos.left + 480 * scale + "px");
+        $("#obtained").css("top", pos.top + 420 * scale + "px");
+        $("#obtained").css("left", pos.left + 220 * scale + "px");
+        $("#status").css("position", "absolute");
+        $("#status").css("top", pos.top + 560 * scale + "px");
+        $("#status").css("left", pos.left + 455 * scale + "px");
+        $("#status").css("width", 360 * scale + "px");
+        $("#status").css("height", 360 * scale + "px");
+        $("#status").css("border-radius", 60 * scale + "px");
+        $("#status").css("opacity", "0.2");
+        // write the values to the display
+        $("#grid").html(gridValue + "W");
+        $("#supply").html(supplyValue + "W");
+        $("#production").html(productionValue + "W");
+        $("#selfuse").html(selfuseValue + "W");
+        $("#consumption").html(consumptionValue + "W");
+        $("#obtained").html(obtainedValue + "W");
+        if (productionValue >= consumptionValue) {
+            $("#status").css("background-color", "green");
         } else {
-            grid.refresh(gridValue);
-        }
-        if (productionValue > limit) {
-            production.refresh(productionValue, productionValue);
-        } else {
-            production.refresh(productionValue);
-        }
-        if (consumptionValue > limit) {
-            consumption.refresh(consumptionValue, consumptionValue);
-        } else {
-            consumption.refresh(consumptionValue);
+            $("#status").css("background-color", "red");
         }
     }
+});
+
+$(document).ready(function() {
+    $("#image").append('<img src="img/EnergyConsumption.png" width=' + $("#image").width() + ' alt="Consumption Graphics" />');
+    $("#toggle").click(function() {
+        if (cfgVis) $("#choices").hide(); else $("#choices").show();
+        cfgVis = !cfgVis;
+    });
 });
