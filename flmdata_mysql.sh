63d1535ea276f82d3f13643300845f88cac58958
Markus Gebhard <markus.gebhard@web.de>
2016-06-04 14:19:07 +0200

    make mysql variant accessible

---
+++ flmdata_mysql.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export AVAHI_COMPAT_NOWARN=1
+# start the script with "no hang-up" to detect potential uncaught errors
+# to have no output catching, just delete the "nohup"
+nohup node serve_flmdata_mysql.js &
